4a2bb55eeff924b34435af94308a461b0de3c849
iamrutra <iamrutra@gmail.com>
2024-08-25 18:00:01 +0200

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>
+                Это приложение работает
+            </h1>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import About from "../../../pages/About";
+import Posts from "../../../pages/Posts";
+import PostIdPage from "../../../pages/PostIdPage";
+
+
+export const route = [
+    {path: '/about', component: About, exact: true},
+    {path: '/posts', component: Posts, exact: true},
+    {path: '/posts/:id', component: PostIdPage, exact: true}
+]
